ff1085ee50d4d430405a2dbd7de744e0967fb6b6
JaysuryaGoski <jaysuryagoski8237@gmail.com>
2024-11-14 08:34:10 +0530

    Dashboard layout done

---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,6 @@
+const DashboardPage = () => {
+    return <div>
+    Dashboard Root Page
+    </div>;
+  };
+  export default DashboardPage;
--- providers/convex-client-provider.tsx
+++ providers/convex-client-provider.tsx
@@ -1,36 +1,29 @@
 "use client";
 
-import { ClerkProvider, useAuth } from "@clerk/nextjs";
+import { useAuth } from "@clerk/nextjs";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
-import {
-  AuthLoading,
-  Authenticated,
-  ConvexReactClient,
-} from "convex/react";
-
-import { Loading } from "@/components/auth/loading"
+import { Authenticated, ConvexReactClient } from "convex/react";
+import { Loading } from "@/components/auth/loading";
 
 interface ConvexClientProviderProps {
   children: React.ReactNode;
-};
+}
 
+// Create Convex client instance using environment variable
 const convexUrl = process.env.NEXT_PUBLIC_CONVEX_URL!;
-
 const convex = new ConvexReactClient(convexUrl);
 
 export const ConvexClientProvider = ({
   children,
 }: ConvexClientProviderProps) => {
   return (
-    <ClerkProvider>
-      <ConvexProviderWithClerk useAuth={useAuth} client={convex}>
-        <Authenticated>
+    <ConvexProviderWithClerk useAuth={useAuth} client={convex}>
+      {/* Use Authenticated component to check if user is logged in */}
+      <Authenticated>
         {children}
-        </Authenticated>
-        <AuthLoading>
-          <Loading />
-        </AuthLoading>
-      </ConvexProviderWithClerk>
-    </ClerkProvider>
+      </Authenticated>
+      {/* Display loading state when user authentication is being checked */}
+      <Loading />
+    </ConvexProviderWithClerk>
   );
 };
